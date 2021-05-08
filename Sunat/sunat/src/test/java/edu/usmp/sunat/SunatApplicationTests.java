d8d2f7953a95f9e54ac4856db44cba874d280aac
Daniel151505 <daniel150520011@gmail.com>
2021-05-08 01:43:56 -0500

    Creación del Proyecto

---
+++ Sunat/sunat/src/test/java/edu/usmp/sunat/SunatApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usmp.sunat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
